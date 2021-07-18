68a36b44eea0693f3ee855e44735139300383aa7
K.K <gokennsirou@yahoo.co.jp>
2021-07-18 11:03:06 +0900

    introduce eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    "browser": true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2021,
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json']
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  },
+  "ignorePatterns": [".eslintrc.js"]
+};
