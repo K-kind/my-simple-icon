e924ac863f1eb4c9cb97e0fef6152d6bf84b149f
Keita Katahira <katahirakeita1201@gmail.com>
2021-07-18 17:21:40 +0900

    Vanilla project end

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+// const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// app.innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+
+import generateSimpleIcon from './lib/generate-simple-icon'
+
+const main = () => {
+  const icon = generateSimpleIcon('#3AD4C4', '#EBFCFA')
+  icon.style.width = '100%'
+  icon.style.height = 'auto'
+  const iconPreviewArea = document.getElementById('icon-preview')!
+  iconPreviewArea.appendChild(icon)
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  main()
+})
